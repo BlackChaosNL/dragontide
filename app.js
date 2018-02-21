4cde1c77287c46338536e4479e455ffc6920c734
Patrick Spek <p.spek@tyil.nl>
2018-02-21 15:40:56 +0100

    Reindent app.js using more sane tabs

--- app.js
+++ app.js
@@ -21,23 +21,23 @@ app.use("/dice", require("./routes/dice"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json({
-    "ok": false,
-    "message": err.message,
-  });
+	// render the error page
+	res.status(err.status || 500);
+	res.json({
+		"ok": false,
+		"message": err.message,
+	});
 });
 
 module.exports = app;
