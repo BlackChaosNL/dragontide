3eda7d36af50fef02002768d299f5cd22f6103e0
Patrick Spek <p.spek@tyil.nl>
2018-02-27 13:25:45 +0100

    Add a swagger.json endpoint

--- routes/dice.js
+++ routes/dice.js
@@ -2,10 +2,29 @@ const express = require('express');
 const router = express.Router();
 const dice = require("../lib/dice");
 
+/**
+ * @swagger
+ * /dice:
+ *   get:
+ *     description: Roll 1d20
+ *     produces: application/json
+ *     response:
+ *       200:
+ *         description: A random number between 1 and 20, inclusive.
+ */
 router.get('/', (req, res, next) => {
 	res.json({"ok": true, "value": dice.rollOne(20)})
 });
 
+/**
+ * @swagger
+ * /dice/{sides}:
+ *   get:
+ *     description: Roll a die with the given number of sides, inclusive.
+ *     responses:
+ *       200:
+ *         description: A random number between 1 and the given maximum.
+ */
 router.get('/:sides', (req, res, next) => {
 	res.json({"ok": true, "value": dice.rollOne(req.params.sides)})
 });
