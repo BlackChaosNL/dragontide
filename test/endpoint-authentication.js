452c4cc62c3ad85b67b969e3f9ae559f9f5aa077
Jeroen Vijgen <jjvijgen@gmail.com>
2018-03-31 18:05:02 +0200

    Fixed tests

--- test/endpoint-authentication.js
+++ test/endpoint-authentication.js
@@ -7,11 +7,29 @@ const request = require("supertest");
 const purge = require("../lib/purge-collection");
 
 before(done => {
-	purge("user");
+	purge("user").then(result => {
+		done();
+	});
 });
 
 describe("Test auth endpoint", () => {
-	it("Can register a player to the service", done => {
+	it("Invalid registration can be caught.", done => {
+		request(app)
+			.post("/auth/register")
+			.send({
+				email: "test@test.xs",
+				password: "Testtesttest"
+			})
+			.expect(200)
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.isFalse(res.body.ok);
+				assert.isString(res.body.message);
+				done();
+			});
+	});
+
+	it("Can register a player to the service.", done => {
 		request(app)
 			.post("/auth/register")
 			.send({
@@ -27,43 +45,56 @@ describe("Test auth endpoint", () => {
 			});
 	});
 
-	it("An active user can login to the service", done => {
-		fake("user").then(users => {
-			request(app)
-				.post("/auth/login")
-				.send({
-					email: users[0].email,
-					password: users[0].password
-				})
-				.expect(200)
-				.end((err, res) => {
-					if (err) return done(err);
-					assert.isTrue(res.body.ok);
-					assert.isString(res.body.message);
-					done();
-				});
-		});
+	it("Duplicate player can not register again.", done => {
+		request(app)
+			.post("/auth/register")
+			.send({
+				email: "test@test.xs",
+				username: "testuser",
+				password: "Testtesttest"
+			})
+			.expect(200)
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.isFalse(res.body.ok);
+				assert.isString(res.body.message);
+				done();
+			});
 	});
 
-	it("Wrong credentials gets caught by the system and refuses to login", done => {
-		fake("user").then(users => {
-			request(app)
-				.post("/auth/login")
-				.send({
-					email: users[0].email,
-					password: users[0].password + "X"
-				})
-				.expect(200)
-				.end((err, res) => {
-					if (err) return done(err);
-					assert.isFalse(res.body.ok);
-					assert.equal(res.body.message, "The username or password was not correct, please try again.");
-					done();
-				});
-		});
+	it("An active user can login to the service.", done => {
+		request(app)
+			.post("/auth/login")
+			.send({
+				email: "test@test.xs",
+				password: "Testtesttest"
+			})
+			.expect(200)
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.isTrue(res.body.ok);
+				assert.isString(res.body.message);
+				done();
+			});
 	});
 
-	it("An active user can logout from the service", done => {
+	it("Wrong credentials gets caught by the system and refuses to login.", done => {
+		request(app)
+			.post("/auth/login")
+			.send({
+				email: "test@test.xs",
+				password: "TesttesttestXXXX"
+			})
+			.expect(200)
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.isFalse(res.body.ok);
+				assert.isString(res.body.message);
+				done();
+			});
+	});
+
+	it("An active user can logout from the service.", done => {
 		fake("user").then(user => {
 			fake("token", 1, {
 				userId: user.id,
@@ -80,6 +111,18 @@ describe("Test auth endpoint", () => {
 					});
 			}).catch(err => done(err));
 		}).catch(err => done(err));
+	});
+
+	it("A non logged in user can not logout.", done => {
+		request(app)
+			.get("/auth/logout")
+			.expect(401)
+			.end((err, res) => {
+				if (err) return done(err);
+				assert.isFalse(res.body.ok);
+				assert.isString(res.body.message);
+				done();
+			});
 	});
 });
 
