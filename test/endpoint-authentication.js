1974799773dab10dc4f56b4ddeb87463b250c386
Jeroen Vijgen <jjvijgen@gmail.com>
2018-03-31 17:03:56 +0200

    Changed tests to be independant

--- test/endpoint-authentication.js
+++ test/endpoint-authentication.js
@@ -6,56 +6,61 @@ const mongoose = require("mongoose");
 const request = require("supertest");
 const purge = require("../lib/purge-collection");
 
+before(done => {
+	purge("user");
+});
+
 describe("Test auth endpoint", () => {
 	it("Can register a player to the service", done => {
-		purge("user")
-			.then(() => {
-				request(app)
-					.post("/auth/register")
-					.send({
-						email: "test@test.xs",
-						username: "testuser",
-						password: "Testtesttest"
-					})
-					.expect(200)
-					.end((err, res) => {
-						if (err) return done(err);
-						assert.isTrue(res.body.ok);
-						done();
-					});
-			});
-	});
-
-	it("An active user can login to the service", done => {
 		request(app)
-			.post("/auth/login")
+			.post("/auth/register")
 			.send({
 				email: "test@test.xs",
+				username: "testuser",
 				password: "Testtesttest"
 			})
 			.expect(200)
 			.end((err, res) => {
 				if (err) return done(err);
 				assert.isTrue(res.body.ok);
-				assert.isString(res.body.message);
 				done();
 			});
 	});
 
+	it("An active user can login to the service", done => {
+		fake("user").then(users => {
+			request(app)
+				.post("/auth/login")
+				.send({
+					email: users[0].email,
+					password: users[0].password
+				})
+				.expect(200)
+				.end((err, res) => {
+					if (err) return done(err);
+					assert.isTrue(res.body.ok);
+					assert.isString(res.body.message);
+					done();
+				});
+		});
+	});
+
 	it("Wrong credentials gets caught by the system and refuses to login", done => {
-		request(app)
-			.post("/auth/login")
-			.send({
-				email: "test@test.xs",
-				password: "Testtestt"
-			})
-			.expect(200)
-			.end((err, res) => {
-				if (err) return done(err);
-				assert.isFalse(res.body.ok);
-				assert.equal(res.body.message, "The username or password was not correct, please try again.");
-				done();
-			});
+		fake("user").then(users => {
+			request(app)
+				.post("/auth/login")
+				.send({
+					email: users[0].email,
+					password: users[0].password + "X"
+				})
+				.expect(200)
+				.end((err, res) => {
+					if (err) return done(err);
+					assert.isFalse(res.body.ok);
+					assert.equal(res.body.message, "The username or password was not correct, please try again.");
+					done();
+				});
+		});
 	});
 
 	it("An active user can logout from the service", done => {
