c1d3a856b4e83d6ec0f5067e37124f46b4059482
Patrick Spek <p.spek@tyil.nl>
2018-04-02 09:11:09 +0200

    Add Model for Item

---
+++ models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.model("Item", new mongoose.Schema({
+	name: String,
+	description: String,
+	weight: Number,
+	stats: {
+		strength: Number,
+		dexterity: Number,
+		constitution: Number,
+		intelligence: Number,
+		wisdom: Number,
+		charisma: Number,
+	},
+	additional: String,
+}));
