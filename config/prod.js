2199b11580db9d113d4cf5f8bfafc8bc2213d6f9
Jeroen Vijgen <BlackChaosNL@users.noreply.github.com>
2018-06-01 00:07:51 +0300

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,4 @@
-let config = {
-	debug: false,
-	db: {
-		host: process.env.username + ":" + process.env.password + "@ds141720.mlab.com:41720",
-		collection: "dragontide",
-	},
-	logging: {
-		requests: true,
-	},
+db: {
+ host: process.env.username + ":" + process.env.password + "@ds141720.mlab.com:41720",
+ collection: "dragontide"
 };
