7520443c9f36f85185dd0233302aab1956f9c348
Jeroen Vijgen <BlackChaosNL@users.noreply.github.com>
2018-06-01 00:34:15 +0300

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,9 @@
 module.exports = {
-db: {
- host: process.env.username + ":" + process.env.password + "@ds141720.mlab.com:41720",
-}
+	debug: false,
+	db: {
+		host: process.env.username + ":" + process.env.password + "@ds141720.mlab.com:41720",
+	},
+	logging: {
+		requests: false,
+	},
 };
